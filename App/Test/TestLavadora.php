97d8366dd25cffaad5243d81c2d7868be677b8d3
Diego Alonso Ojeda Medina <dojedam@sena.edu.co>
2021-04-09 08:02:36 -0500

    Constructor, Destructor y toString

--- App/Test/TestLavadora.php
+++ App/Test/TestLavadora.php
@@ -3,8 +3,13 @@
 include "../Models/Lavadora.php";
 use App\Models\Lavadora;
 
+echo "<pre>";
+
 $lavadoraLG = new Lavadora();
 $lavadoraLG->setMarca("LG");
 $lavadoraLG->setColor("Negro");
-
 echo "La lavadora ".$lavadoraLG->getMarca()." es de color: ".$lavadoraLG->getColor();
+$lavadoraSamsung = new Lavadora("Verde","Samsung",123456, 32.5,"2021-01-01",300);
+echo $lavadoraSamsung;
+
+echo "</pre>";
